21d280892569c8c795674e677d138ddf7c0b54f7
kageyama ginn <hank.han@pery.com>
2018-05-10 20:40:27 +0800

    Initial repository

---
+++ DataTaker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataTaker.MvcApplication" Language="C#" %>
---
+++ TestClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using DbDataHelper.Oracle;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Table t = new Table();
+            t.TableName = "SNT_FI_AUDIT";
+            t.Owner = "PLM";
+
+            Type tableType = typeof(Table);
+
+            List<Attribute> atts = tableType.GetCustomAttributes().ToList() ;
+            for (int attIndex = 0; attIndex < atts.Count(); attIndex++)
+            {
+                Type attType = atts[attIndex].GetType();
+                
+
+            }
+            
+        }
+    }
+}
